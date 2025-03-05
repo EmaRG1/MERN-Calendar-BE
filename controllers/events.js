a42da012e81850bd41f9097a9cd63af17eb55870
Emanuel <emanuelrojasguindan@gmail.com>
2025-03-05 20:47:30 -0300

    create event and get all events logic

--- controllers/events.js
+++ controllers/events.js
@@ -1,29 +1,59 @@
-const express = require('express');
+const {response} = require('express');
+const Event = require('../models/EventModel')
 
-const getAllEvents = (req, res) => {
-  res.json({
-    ok: true,
-    msg: 'getAllEvents'
-  })
+const getAllEvents = async (req, res = response) => {
+
+  try {
+    const allEvents = await Event.find({}).populate('user','name')
+    res.json({
+      ok: true,
+      allEvents
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Failed to retrieve events'
+    })
+  }
 }
 
-const createEvent = (req, res) => {
+const createEvent = async (req, res = response) => {
+  
+  const newEvent = new Event(req.body)
 
-  console.log(req.body)
-  res.json({
-    ok: true,
-    msg: 'createEvent'
-  })
+  try {
+
+    newEvent.user = req.uid
+
+    const eventDB = await newEvent.save()
+
+    res.status(201).json({
+      ok: true,
+      eventDB
+    })
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Failed to create event'
+    })
+
+  }
+  
 }
 
-const updateEvent = (req, res) => {
+const updateEvent = (req, res = response) => {
   res.json({
     ok: true,
     msg: 'updateEvent'
   })
 }
 
-const deleteEvent = (req, res) => {
+const deleteEvent = (req, res = response) => {
   res.json({
     ok: true,
     msg: 'deleteEvent'
